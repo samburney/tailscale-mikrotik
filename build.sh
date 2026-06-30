63feab70f9ca4c1a810d37aba938e7fc7db28165
Sam Burney <sburney@sifnt.net.au>
2026-06-30 21:16:16 +0930

    Minor `build.sh` convenience enhancements;  * Script defaults remain unchanged  * Allow `PLATFORM` to be specified from environment  * Use `default` builder when target and host architecture match  * Include arch and tailscale version in image filename  * Allow arbitrary `docker buildx` arguments to be specified as arguments  * Added `--use-cache` argument to negate default `--no-cache`  * Added tailscale version to image tag  * Attempt to create builder if it is missing

--- build.sh
+++ build.sh
@@ -26,13 +26,48 @@
 # Set PLATFORM as required for your router model. See:
 # https://mikrotik.com/products/matrix
 #
-PLATFORM="linux/arm64"
+PLATFORM="${PLATFORM:-linux/arm64}"
 TAILSCALE_VERSION=1.96.3
 VERSION=0.1.40
 
+# Set other script values to be platform-specific
+HOST_PLATFORM=$(docker version -f '{{.Server.Os}}/{{.Server.Arch}}')
+PLATFORM_COMPAT=$(echo "$PLATFORM" | sed -E 's/[^-0-9a-z]/-/gI; s/-+/-/g')
+FILENAME="tailscale-${TAILSCALE_VERSION}-${PLATFORM_COMPAT}.tar"
+
+# Determine if a cross-platform builder is required
+if [ "$PLATFORM" = "$HOST_PLATFORM" ]; then
+  BUILDER="default"
+else
+  BUILDER="${PLATFORM_COMPAT}-builder"
+  if ! docker buildx inspect "$BUILDER" >/dev/null 2>&1; then
+    docker buildx create --name "${BUILDER}" --platform "${PLATFORM}" --use
+  fi
+fi
+
+# Accept additional `docker buildx build` flags, defaulting to `--no-cache`.
+# `--use-cache` is negates the default `--no-cache`.
+USE_CACHE=0
+ARGC=$#
+i=0
+while [ "$i" -lt "$ARGC" ]; do
+  arg="$1"
+  shift
+  if [ "$arg" = "--use-cache" ]; then
+    USE_CACHE=1
+  else
+    set -- "$@" "$arg"
+  fi
+  i=$((i + 1))
+done
+
+if [ "$USE_CACHE" -eq 0 ] && [ "$#" -eq 0 ]; then
+  set -- --no-cache
+fi
+
 set -eu
 
-rm -f tailscale.tar
+rm -f "${FILENAME}"
 
 if [ ! -d ./tailscale/.git ]
 then
@@ -43,13 +78,13 @@ TS_USE_TOOLCHAIN="Y"
 cd tailscale && eval $(./build_dist.sh shellvars) && cd ..
 
 docker buildx build \
-  --no-cache \
+  "$@" \
   --build-arg TAILSCALE_VERSION=$TAILSCALE_VERSION \
   --build-arg VERSION_LONG=$VERSION_LONG \
   --build-arg VERSION_SHORT=$VERSION_SHORT \
   --build-arg VERSION_GIT_HASH=$VERSION_GIT_HASH \
   --platform $PLATFORM \
-  --builder arm64-builder \
-  --load -t ghcr.io/fluent-networks/tailscale-mikrotik:$VERSION .
+  --builder $BUILDER \
+  --load -t ghcr.io/fluent-networks/tailscale-mikrotik:$VERSION-$TAILSCALE_VERSION .
 
-skopeo copy docker-daemon:ghcr.io/fluent-networks/tailscale-mikrotik:$VERSION docker-archive:tailscale.tar
+skopeo copy docker-daemon:ghcr.io/fluent-networks/tailscale-mikrotik:$VERSION-$TAILSCALE_VERSION docker-archive:"$FILENAME"
